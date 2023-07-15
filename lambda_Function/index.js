d57946dde755b758276508b785df212396f200ca
Shu12388y <spl.sp999@gmail.com>
2023-07-15 15:15:09 +0530

    file modified

--- lambda_Function/index.js
+++ lambda_Function/index.js
@@ -4,7 +4,7 @@
 
 const message=["hello world","New World","Nice to see you"];
 
-export.handler=async(event,context)=>{
+exports.handler=async(event,context)=>{
   let ms=message[Math.floor(Math.random().message.length)]
   return ms;
 
